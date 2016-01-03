590fad5cc2eef5633bd14a3a4f758ac6c3ae570d
Andrew Rea <email@andrewrea.co.uk>
2016-01-03 00:17:31 +0000

    Creating a basic http server

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-var util = require('util');
+const http = require('http');
 
-util.log('Hello World!');
+const hostname = '0.0.0.0';
+const port = 1337;
+
+const todos = [
+	'Buy some milk',
+	'Book appointment with the dentist',
+	'Post the letter'
+];
+
+http.createServer((req, res) => {
+  console.log(req.url);
+  if(req.url === '/todos'){
+     res.writeHead(200, { 'Content-Type': 'text/plain' });
+     res.end(todos.join('\n')+'\n');
+  }else{
+     res.writeHead(404);
+     res.end();   
+  }
+}).listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
