ab1c606334207b0346c2dacf724e39994287f30d
Andrew Rea <email@andrewrea.co.uk>
2016-01-07 16:59:52 +0000

    Updated the index.js to support creating a new todo with the POST method

--- index.js
+++ index.js
@@ -3,20 +3,50 @@ const http = require('http');
 const hostname = '0.0.0.0';
 const port = 1337;
 
-const todos = [
-	'Buy some milk',
-	'Book appointment with the dentist',
-	'Post the letter'
-];
+const todos = [{
+	id : 1,
+  title: 'Buy some milk',
+  complete: false
+}, {
+	id : 2,
+  title: 'Book appointment with the dentist',
+  complete: false
+}, {
+	id : 3,
+  title: 'Post the letter',
+  complete: false
+}];
 
 http.createServer((req, res) => {
-  console.log(req.url);
-  if(req.url === '/todos'){
-     res.writeHead(200, { 'Content-Type': 'text/plain' });
-     res.end(todos.join('\n')+'\n');
-  }else{
-     res.writeHead(404);
-     res.end();   
+  console.log(req);
+  if (req.url === '/todos') {
+    if (req.method == 'POST') {
+			var payload = new Buffer(0);
+
+			req.on('data', (data) => {
+				payload = Buffer.concat([payload, data]);
+			});
+
+			req.on('end', () => {
+					const todo = JSON.parse(payload);
+					todo.id = todos.length + 1;
+					todo.complete = false;
+					todos.push(todo);
+					res.writeHead(201, {
+		        'Content-Type': 'application/json',
+						'Location' : `/todos/${todo.id}`
+		      });
+		      res.end();
+			});
+    } else {
+      res.writeHead(200, {
+        'Content-Type': 'application/json'
+      });
+      res.end(JSON.stringify(todos));
+    }
+  } else {
+    res.writeHead(404);
+    res.end();
   }
 }).listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
